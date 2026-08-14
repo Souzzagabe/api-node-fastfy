ef0484c3e647731035ea451f71aabed8977807ae
Souza <gabweb95@gmail.com>
2026-08-14 17:21:13 -0300

    fix(auth): update login response schema for HttpOnly cookie

--- schemas.js
+++ schemas.js
@@ -1,24 +1,34 @@
 export const schemas = {
   Error: {
     $id: 'Error',
+
     type: 'object',
+
     properties: {
       message: {
         type: 'string',
       },
     },
+
     required: ['message'],
   },
 
   CreateUser: {
     $id: 'CreateUser',
+
     type: 'object',
-    required: ['username', 'password'],
+
+    required: [
+      'username',
+      'password',
+    ],
+
     properties: {
       username: {
         type: 'string',
         minLength: 3,
       },
+
       password: {
         type: 'string',
         minLength: 6,
@@ -28,27 +38,41 @@ export const schemas = {
 
   UserResponse: {
     $id: 'UserResponse',
+
     type: 'object',
+
     properties: {
       id: {
         type: 'string',
         format: 'uuid',
       },
+
       username: {
         type: 'string',
       },
     },
-    required: ['id', 'username'],
+
+    required: [
+      'id',
+      'username',
+    ],
   },
 
   Login: {
     $id: 'Login',
+
     type: 'object',
-    required: ['username', 'password'],
+
+    required: [
+      'username',
+      'password',
+    ],
+
     properties: {
       username: {
         type: 'string',
       },
+
       password: {
         type: 'string',
       },
@@ -57,26 +81,34 @@ export const schemas = {
 
   LoginResponse: {
     $id: 'LoginResponse',
+
     type: 'object',
+
     properties: {
-      token: {
+      message: {
         type: 'string',
       },
     },
-    required: ['token'],
+
+    required: ['message'],
   },
 
   CreateTodo: {
     $id: 'CreateTodo',
+
     type: 'object',
+
     required: ['title'],
+
     properties: {
       title: {
         type: 'string',
       },
+
       description: {
         type: 'string',
       },
+
       completed: {
         type: 'boolean',
         default: false,
@@ -86,21 +118,27 @@ export const schemas = {
 
   Todo: {
     $id: 'Todo',
+
     type: 'object',
+
     properties: {
       id: {
         type: 'string',
         format: 'uuid',
       },
+
       title: {
         type: 'string',
       },
+
       description: {
         type: 'string',
       },
+
       completed: {
         type: 'boolean',
       },
+
       created_at: {
         type: 'string',
         format: 'date-time',
@@ -110,24 +148,30 @@ export const schemas = {
 
   CreatedResponse: {
     $id: 'CreatedResponse',
+
     type: 'object',
+
     properties: {
       id: {
         type: 'string',
         format: 'uuid',
       },
     },
+
     required: ['id'],
   },
 
   MessageResponse: {
     $id: 'MessageResponse',
+
     type: 'object',
+
     properties: {
       message: {
         type: 'string',
       },
     },
+
     required: ['message'],
   },
-};
+}
